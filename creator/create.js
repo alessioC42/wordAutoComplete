826ff6a09c39557e544079d37cbfc5d8d22e6071
Alessio Caputo <84250128+alessioC42@users.noreply.github.com>
2023-07-30 12:45:00 +0200

    JS create script

---
+++ creator/create.js
@@ -0,0 +1,92 @@
+function probabilityWithStart(start, wordDict, words) {
+  let dictCount = {};
+  let lenStart = start.length;
+
+  if (lenStart > 0 && start[lenStart - 1] == " ") return [[], []];
+
+  let validWords = words.filter((word) => word.startsWith(start));
+
+  for (let word of validWords) {
+    if (word[lenStart] in dictCount) {
+      dictCount[word[lenStart]] += wordDict[word];
+    } else {
+      dictCount[word[lenStart]] = wordDict[word];
+    }
+  }
+  return [
+    Object.keys(dictCount).sort((a, b) => dictCount[b] - dictCount[a]),
+    validWords,
+  ];
+}
+
+function getAllFollowingChars(start, words, wordDict) {
+  let returnObj = [];
+
+  let [chars, validWords] = probabilityWithStart(start, wordDict, words);
+
+  for (let i = 0; i < chars.length; i++) {
+    let char = chars[i];
+    let appendObj = {
+      l: char,
+      p: i,
+      f: getAllFollowingChars(start + char, validWords, wordDict),
+    };
+    returnObj.push(appendObj);
+  }
+  return returnObj;
+}
+
+function generateModel(text) {
+  text = text.toLowerCase();
+  text = text.replace(/[.,\/#?!$%\^&\*;:{}\r=\-_`~()«»©]/g, "");
+  text = text.replace(/\n/g, " ");
+
+  let words = text.split(" ");
+
+  words = words.filter((x) => x != "");
+  words = words.map((x) => x + " ");
+  let wordDict = {};
+  for (let word of words) {
+    if (word in wordDict) {
+      wordDict[word]++;
+    } else {
+      wordDict[word] = 1;
+    }
+  }
+  words = Object.keys(wordDict);
+
+  console.log(
+    "Generating model with a total of " + words.length + " different words"
+  );
+
+  //generate JSON
+  let returnObj = [];
+  let [chars, _x] = probabilityWithStart("", wordDict, words);
+  for (let i = 0; i < chars.length; i++) {
+    let char = chars[i];
+    let appendObj = {
+      l: char,
+      p: i,
+      f: getAllFollowingChars(char, words, wordDict),
+    };
+    returnObj.push(appendObj);
+  }
+
+  return (returnObj);
+}
+
+function getPromise(model, input) {
+    word = input.value.split(" ").pop().toLowerCase()
+
+    for (let i = 0; i < word.length; i++) {
+        model = model.find(node => node.l === word[i]);
+        if (!model) return word;
+        model = model.f;
+    }
+    while (model && model[0].l !== ' ') {
+        model = model.reduce((a, b) => a.p < b.p ? a : b);
+        word += model.l;
+        model = model.f;
+    }
+    return word;
+}
